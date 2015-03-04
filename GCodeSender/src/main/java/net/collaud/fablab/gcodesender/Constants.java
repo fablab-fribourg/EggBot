cbd5e6df3143dfc35a3717a86ca0497da0e578a1
Gaétan Collaud <gaetancollaud@gmail.com>
2015-03-04 10:16:44 +0100

    reduce servo waiting time from 400 to 200ms

--- GCodeSender/src/main/java/net/collaud/fablab/gcodesender/Constants.java
+++ GCodeSender/src/main/java/net/collaud/fablab/gcodesender/Constants.java
@@ -6,5 +6,5 @@ package net.collaud.fablab.gcodesender;
  */
 public interface Constants {
 	public static final int ARDUINO_READY_TIMEOUT_MS = 5000;
-	public static final int M_COMMAND_WAIT = 400;
+	public static final int M_COMMAND_WAIT = 200;
 }
