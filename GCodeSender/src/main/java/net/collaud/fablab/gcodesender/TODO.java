6f2cb21902f9a905cce00ab7b3b2091f11686609
gaetancollaud <gaetancollaud@gmail.com>
2015-03-05 21:33:34 +0100

    TODO list

---
+++ GCodeSender/src/main/java/net/collaud/fablab/gcodesender/TODO.java
@@ -0,0 +1,18 @@
+package net.collaud.fablab.gcodesender;
+
+/**
+ *
+ * @author Gaetan Collaud <gaetancollaud@gmail.com>
+ */
+public interface TODO {
+	
+	/**
+	 * - unit test with some real gcode
+	 * - save last port used and last file used
+	 * - alert whith boundary
+	 * - compute scale
+	 * - use scale to scale down image
+	 */
+		
+		
+}
